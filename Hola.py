a93510c0aaadfecf0e43e5cf8e5f421dbf473114
alejandrobelloc <69279675+alejandrobelloc@users.noreply.github.com>
2020-08-05 23:08:18 -0400

    Add files via upload

---
+++ Hola.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+nombre = "Alejandro"
+dia = 30
+
+print("Forma 1")
+print("hola Alejandro es el 30 de Agosto")
+
+
+print("Forma 2")
+print("hola " + nombre + " es el " + str(dia) + " de Agosto")
+
+print("Forma 3")
+print("hola {nombre} es el {dia} de Agosto".format(dia=dia, nombre=nombre))
+
+print("Forma 4")
+print(f"hola {nombre} es el {dia} de Agosto")
+
+print("Ejemplo 2 - Forma 1")
+print("Alejandro Alejandro Alejandro Alejandro Alejandro Alejandro Alejandro Alejandro Alejandro ")
+
+print("Ejemplo 2 - Forma 1")
+print("Alejandro "*9)
